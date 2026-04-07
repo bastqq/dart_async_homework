5719f224bc07050ebf3d4d6a69d2c7f6d3fdf332
bastqq <bastqq@gmail.com>
2026-04-07 19:59:05 +0300

    feat-DART-11-homework part 2

--- lib/homework/lesson_11/hw_futures.dart
+++ lib/homework/lesson_11/hw_futures.dart
@@ -4,6 +4,30 @@ void main() async {
     return 'Roman';
   }
 
-  String res = await fetchName();
-  print('Мене звати $res');
+  String name = await fetchName();
+  print('Мене звати $name');
+  print('________________end_task_1_______________');
+
+  Future<String> fetchAge(int age) async {
+    await Future.delayed(Duration(milliseconds: 1500));
+    return age.toString();
+  }
+
+  String ageString = await fetchAge(13);
+  int ageInt = int.parse(ageString);
+  String ageEnding = '';
+  List<int> ageNumbers = ageInt.toString().split('').map(int.parse).toList();
+  final pokuList = [2, 3, 4];
+  final notPokuList = [11, 12, 13, 14];
+  if (ageNumbers.last == 1 && ageInt != 11) {
+    ageEnding = 'рік';
+  } else if (pokuList.contains(ageNumbers.last) &&
+      !notPokuList.contains(ageInt)) {
+    ageEnding = 'роки';
+  } else {
+    ageEnding = 'років';
+  }
+
+  print("Мені $ageString $ageEnding");
+  print('________________end_task_2_______________');
 }
